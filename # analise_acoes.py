ca8478f3f0eb8dadacfa106948d6ceed70dc17c2
diegoPaladino <diegopaladinofoto@gmail.com>
2021-07-17 18:21:18 -0300

    202107171821

---
+++ # analise_acoes.py
@@ -0,0 +1,6 @@
+# analise_acoesimport pandas as pd
+from pandas_datareader import data as web
+import plotly.graph_objects as go
+ 
+# criar um DataFrame vazio
+df = pd.DataFrame()
---
+++ collecting_stock_prices.py
@@ -0,0 +1,16 @@
+# collecting_stock_prices
+# COMO COLETAR OS CLOSES DE VÁRIOS ATIVOS EM DATAFRAME - PYTHON E MT5
+# font: https://www.youtube.com/watch?v=f8BsjwOLHzE
+
+
+# importing libraries
+import MetaTrader5 as MT5
+import time as t
+import pandas as pd
+
+
+# declarating functions
+
+
+# code execution
+
